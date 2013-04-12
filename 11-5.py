bcc231ac57eebdef15291acecee106906884ba97
Jenna Telesca <telesca@Jenna-Telescas-MacBook-Air.local>
2013-04-12 00:26:43 -0400

    Exercise 11-5

---
+++ 11-5.py
@@ -0,0 +1,16 @@
+print "What is your favorite color?",
+color = raw_input()
+
+# the comma keeps print from making a line break
+
+print "%s is a nice color." % (color)
+print "What is your favorite movie?",
+movie = raw_input ()
+print "%s is a crappy movie." % (movie)
+
+print "Okay, what town do you live in?",
+town = raw_input()
+print "%s? Bleck. Get outta here." % (town)
+
+
+
